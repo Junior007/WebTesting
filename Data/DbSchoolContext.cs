4baf0678d667a532a064a997d5a570c86fce8f8c
Junior007 <agilgomez@gmail.com>
2017-07-31 16:54:20 +0200

    InitialLoad

---
+++ Data/DbSchoolContext.cs
@@ -0,0 +1,77 @@
+namespace SchoolDataBase
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class DbSchoolContext : DbContext
+    {
+        public DbSchoolContext(string connectionString)
+            : base(connectionString)
+        {
+            
+        }
+
+        public virtual DbSet<Course> Courses { get; set; }
+        public virtual DbSet<Department> Departments { get; set; }
+        public virtual DbSet<OfficeAssignment> OfficeAssignments { get; set; }
+        public virtual DbSet<OnlineCourse> OnlineCourses { get; set; }
+        public virtual DbSet<OnsiteCourse> OnsiteCourses { get; set; }
+        public virtual DbSet<Person> Instructors { get; set; }
+        public virtual DbSet<StudentGrade> StudentGrades { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>()
+                .HasOptional(e => e.OnlineCourse)
+                .WithRequired(e => e.Course);
+
+            modelBuilder.Entity<Course>()
+                .HasOptional(e => e.OnsiteCourse)
+                .WithRequired(e => e.Course);
+
+            modelBuilder.Entity<Course>()
+                .HasMany(e => e.StudentGrades)
+                .WithRequired(e => e.Course)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Course>()
+                .HasMany(e => e.Instructors)
+                .WithMany(e => e.Courses)
+                .Map(m => m.ToTable("CourseInstructor").MapLeftKey("CourseID").MapRightKey("PersonID"));
+
+            //modelBuilder.Entity<Course>()
+            //    .HasMany(e => e.Student)
+            //    .WithMany(e => e.Courses)
+            //    .Map(m => m.ToTable("StudentGrade").MapLeftKey("CourseID").MapRightKey("PersonID"));
+
+            modelBuilder.Entity<Department>()
+                .Property(e => e.Budget)
+                .HasPrecision(19, 4);
+
+            modelBuilder.Entity<Department>()
+                .HasMany(e => e.Courses)
+                .WithRequired(e => e.Department)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<OfficeAssignment>()
+                .Property(e => e.Timestamp)
+                .IsFixedLength();
+
+            modelBuilder.Entity<Person>()
+                .HasOptional(e => e.OfficeAssignment)
+                .WithRequired(e => e.Person);
+
+            modelBuilder.Entity<Person>()
+                .HasMany(e => e.StudentGrades)
+                .WithRequired(e => e.Person)
+                .HasForeignKey(e => e.StudentID)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<StudentGrade>()
+                .Property(e => e.Grade)
+                .HasPrecision(3, 2);
+        }
+    }
+}
---
+++ Data/Repository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+//using System.Data.Objects;
+//using System.Data.Objects.DataClasses;
+//https://msdn.microsoft.com/es-es/library/bb399731(v=vs.100).aspx
+namespace SchoolDataBase
+{
+    public class Repository
+    {
+        private DbSchoolContext dbSchoolContext;
+        public Repository()
+        {
+            dbSchoolContext = new DbSchoolContext(conectionString);
+        }
+
+        private string conectionString
+        {
+            get
+            {
+                return @"data source=(localdb)\MSSQLLocalDB;initial catalog=School;integrated security=True;MultipleActiveResultSets=True;App=EntityFramework";// System.Configuration.ConfigurationManager.ConnectionStrings["School"].ConnectionString;
+            }
+        }
+
+        public IList<Course> getCourses()
+        {
+            return dbSchoolContext.Courses.ToList<Course>();
+
+        }
+
+        public Course getCourse(int courseID)
+        {
+            return dbSchoolContext.Courses.Where(x => x.CourseID == courseID).FirstOrDefault<Course>();
+        }
+    }
+}
